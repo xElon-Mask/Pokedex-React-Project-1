337408362ae1e530a0ea5aca6290a2acb50861fb
xElon-Mask <xelonmask@protonmail.com>
2022-07-06 17:17:55 +0200

    stylize cards + add props on the React components

---
+++ src/components/pokemon-card.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from 'react';
+import Pokemon from '../models/pokemon';
+import './pokemon-card.css';
+  
+type Props = {
+  pokemon: Pokemon,
+  borderColor?: string
+};
+  
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
+    
+  return (
+    <div className="col s6 m4">
+      <div className="card horizontal" style={{ borderColor: borderColor}}>
+        <div className="card-image"> 
+          <img src={pokemon.picture} alt={pokemon.name}/>
+        </div>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p>{pokemon.name}</p>
+            <p><small>{pokemon.created.toString()}</small></p>
+          </div>
+        </div>
+      </div> 
+    </div>
+  );
+}
+  
+export default PokemonCard;
